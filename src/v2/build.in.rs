a747768a215176d11b8b6cdef1df325c4db1db5b
Eric Kidd <git@randomhacks.net>
2016-08-20 10:17:38 -0400

    Add `dockerfile` field to `Build`

--- src/v2/build.in.rs
+++ src/v2/build.in.rs
@@ -7,6 +7,9 @@
 pub struct Build {
     /// The source directory to use for this build.
     pub context: Context,
+
+    /// The name of an alternate `Dockerfile` to use.
+    pub dockerfile: Option<String>,
 }
 
 // This hideous deserializer handles the fact that `build:` can be
@@ -16,23 +19,33 @@ impl Deserialize for Build {
     fn deserialize<D>(deserializer: &mut D) -> Result<Build, D::Error>
         where D: Deserializer
     {
+        // We create a `Visitor` type, with one method for each data type
+        // we support.  The deserializer will call the method corresponding
+        // to the data that's actually in the file.
         struct BuildVisitor;
 
         impl Visitor for BuildVisitor {
             type Value = Build;
 
+            // The deserializer found a string, so handle it.
             fn visit_str<E>(&mut self, value: &str) -> Result<Build, E>
                 where E: de::Error
             {
                 Ok(Build {
                     context: Context::new(value),
+                    dockerfile: None,
                 })
             }
 
+            // The deserializer found a key/value map.  We'll need to
+            // extract the keys and values one at a time and turn them into
+            // an object.
             fn visit_map<V>(&mut self, mut visitor: V) -> Result<Build, V::Error>
                 where V: MapVisitor
             {
                 let mut context: Option<Context> = None;
+                let mut dockerfile: Option<String> = None;
+
                 while let Some(key) = try!(visitor.visit_key::<String>()) {
                     match key.as_ref() {
                         "context" => {
@@ -40,6 +53,12 @@ impl Deserialize for Build {
                                 return Err(<V::Error as Error>::duplicate_field("context"));
                             }
                             context = Some(try!(visitor.visit_value()));
+                        }
+                        "dockerfile" => {
+                            if dockerfile.is_some() {
+                                return Err(<V::Error as Error>::duplicate_field("dockerfile"));
+                            }
+                            dockerfile = Some(try!(visitor.visit_value()));
                         }
                         name => {
                             return Err(<V::Error as Error>::unknown_field(name));
@@ -53,6 +72,7 @@ impl Deserialize for Build {
                 };
                 Ok(Build {
                     context: context,
+                    dockerfile: dockerfile,
                 })
             }
         }
@@ -66,10 +86,16 @@ impl Deserialize for Build {
 fn build_may_be_a_bare_string() {
     let build: Build = serde_yaml::from_str("---\n\".\"").unwrap();
     assert_eq!(build.context, Context::new("."));
+    assert_eq!(build.dockerfile, None);
 }
 
 #[test]
 fn build_may_be_a_struct() {
-    let build: Build = serde_yaml::from_str("---\ncontext: \".\"").unwrap();
+    let yaml = "---
+context: \".\"
+dockerfile: \"Dockerfile\"
+";
+    let build: Build = serde_yaml::from_str(yaml).unwrap();
     assert_eq!(build.context, Context::new("."));
+    assert_eq!(build.dockerfile, Some("Dockerfile".to_owned()));
 }
