a68544a9f0ac182a83a645f1caa776d3a42dc36d
Eric Kidd <git@randomhacks.net>
2016-08-20 08:36:51 -0400

    Deserialize `build:` with a simple string argument
    This code is much worse than I hoped for.

--- src/serde_types.in.rs
+++ src/serde_types.in.rs
@@ -11,7 +11,8 @@
 // ```
 
 use regex::Regex;
-use serde::de::{Deserialize, Deserializer};
+use serde::Error;
+use serde::de::{self, Deserialize, Deserializer, MapVisitor, Visitor};
 use serde::ser::{self, Serialize, Serializer};
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
@@ -48,10 +49,75 @@ pub struct Service {
 }
 
 /// Information on how to build
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Serialize)]
 pub struct Build {
     /// The source directory to use for this build.
     pub context: Context,
+}
+
+// This hideous deserializer handles the fact that `build:` can be
+// serialized as either a bare context string, or as a structure with
+// multiple nested keys.
+impl Deserialize for Build {
+    fn deserialize<D>(deserializer: &mut D) -> Result<Build, D::Error>
+        where D: Deserializer
+    {
+        struct BuildVisitor;
+
+        impl Visitor for BuildVisitor {
+            type Value = Build;
+
+            fn visit_str<E>(&mut self, value: &str) -> Result<Build, E>
+                where E: de::Error
+            {
+                Ok(Build {
+                    context: Context::new(value),
+                })
+            }
+
+            fn visit_map<V>(&mut self, mut visitor: V) -> Result<Build, V::Error>
+                where V: MapVisitor
+            {
+                let mut context: Option<Context> = None;
+                while let Some(key) = try!(visitor.visit_key::<String>()) {
+                    match key.as_ref() {
+                        "context" => {
+                            if context.is_some() {
+                                return Err(<V::Error as Error>::duplicate_field("context"));
+                            }
+                            context = Some(try!(visitor.visit_value()));
+                        }
+                        name => {
+                            return Err(<V::Error as Error>::unknown_field(name));
+                        }
+                    }
+                }
+                try!(visitor.end());
+                let context = match context {
+                    Some(context) => context,
+                    None => try!(visitor.missing_field("context")),
+                };
+                Ok(Build {
+                    context: context,
+                })
+            }
+        }
+
+        const FIELDS: &'static [&'static str] = &["context"];
+        deserializer.deserialize_struct("Build", FIELDS, BuildVisitor)
+    }
+}
+
+#[test]
+fn build_may_be_a_bare_string() {
+    let build: Build = serde_yaml::from_str("---\n\".\"").unwrap();
+    assert_eq!(build.context, Context::new("."));
+}
+
+#[test]
+fn build_may_be_a_struct() {
+    let build: Build = serde_yaml::from_str("---\ncontext: \".\"").unwrap();
+    assert_eq!(build.context, Context::new("."));
 }
 
 /// Either a local directory path, or a Git-format "URL" (not necessarily a
@@ -63,6 +129,26 @@ pub enum Context {
     /// A Git repository, specified using any of the usual git repository
     /// syntaxes.
     GitUrl(String),
+}
+
+impl Context {
+    /// Construct a new Context from a string, identifying it as either a
+    /// local path or a remote git repository.
+    fn new(s: &str) -> Context {
+        // Compile our regex just once.  There's a nice macro for this if
+        // we're using nightly Rust, but lazy_static works on stable.
+        lazy_static! {
+            // See http://stackoverflow.com/a/34120821/12089
+            static ref GIT_PREFIX: Regex =
+                Regex::new("^(https?://|git://|github.com/|git@)").unwrap();
+        }
+
+        if GIT_PREFIX.is_match(&s) {
+            Context::GitUrl(s.to_owned())
+        } else {
+            Context::Dir(Path::new(&s).to_owned())
+        }
+    }
 }
 
 // Custom serializer to output both directories and git URLs as strings.
@@ -91,20 +177,8 @@ impl Deserialize for Context {
     fn deserialize<D>(deserializer: &mut D) -> Result<Context, D::Error>
         where D: Deserializer
     {
-        // Compile our regex just once.  There's a nice macro for this if
-        // we're using nightly Rust.
-        lazy_static! {
-            // See http://stackoverflow.com/a/34120821/12089
-            static ref GIT_PREFIX: Regex =
-                Regex::new("^(https?://|git://|github.com/|git@)").unwrap();
-        }
-
         let s: String = try!(Deserialize::deserialize(deserializer));
-        if GIT_PREFIX.is_match(&s) {
-            Ok(Context::GitUrl(s))
-        } else {
-            Ok(Context::Dir(Path::new(&s).to_owned()))
-        }
+        Ok(Context::new(&s))
     }
 }
 
