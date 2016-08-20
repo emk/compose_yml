9cbafe8656d87b78fe0ecf2f9294d2f21077d4f9
Eric Kidd <git@randomhacks.net>
2016-08-20 09:02:15 -0400

    Allow serde types to appear in multiple files
    We're going to have so many custom serializers/deserializers that we'll
    want to break them into multiple files.

--- build.rs
+++ build.rs
@@ -4,10 +4,12 @@
 // Rust code before the compiler sees it.  But when we build under nightly
 // builds, we let the compiler do its thing normally.
 //
-// Copied from https://serde.rs/codegen-hybrid.html.
+// Copied from https://serde.rs/codegen-hybrid.html and adapted to support
+// multiple input files, because we have a ton of data structures.
 
 #[cfg(feature = "serde_codegen")]
 fn main() {
+    extern crate glob;
     extern crate serde_codegen;
 
     use std::env;
@@ -15,10 +17,26 @@ fn main() {
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
 
-    let src = Path::new("src/serde_types.in.rs");
-    let dst = Path::new(&out_dir).join("serde_types.rs");
+    // Switch to our `src` directory so that we have the right base for our
+    // globs, and so that we won't need to strip `src/` off every path.
+    env::set_current_dir("src").unwrap();
 
-    serde_codegen::expand(&src, &dst).unwrap();
+    for entry in glob::glob("**/*.in.rs").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(src) => {
+                let mut dst = Path::new(&out_dir).join(&src);
+
+                // Change ".in.rs" to ".rs".
+                dst.set_file_name(src.file_stem().expect("Failed to get file stem"));
+                dst.set_extension("rs");
+
+                serde_codegen::expand(&src, &dst).unwrap();
+            }
+            Err(e) => {
+                panic!("Error globbing: {}", e);
+            }
+        }
+    }
 }
 
 #[cfg(not(feature = "serde_codegen"))]
