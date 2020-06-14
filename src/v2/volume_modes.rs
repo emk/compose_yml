0622c2928a7130d5699bfbebe186d09ca21f0b74
dvejmz <dvejmz@gmail.com>
2020-06-14 16:48:02 +0100

    Rename VolumePermissions to VolumeModes
    This more accurately reflects the different modes a volume can be
    mounted into the container, which not only includes whether it's
    read-only or not but also data consistency configurations between the
    host and the container.

--- src/v2/volume_modes.rs
+++ src/v2/volume_modes.rs
@@ -1,50 +1,50 @@
 use super::common::*;
 
-/// Permissions on volumes that are mapped into the Docker container.
+/// Mount modes on volumes that are mapped into the Docker container.
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
-pub enum VolumePermissions {
+pub enum VolumeModes {
     /// This volume can be read and written (default).
     ReadWrite,
     /// This volume is ready-only.
     ReadOnly,
 }
 
-impl Default for VolumePermissions {
-    fn default() -> VolumePermissions {
-        VolumePermissions::ReadWrite
+impl Default for VolumeModes {
+    fn default() -> VolumeModes {
+        VolumeModes::ReadWrite
     }
 }
 
-impl fmt::Display for VolumePermissions {
+impl fmt::Display for VolumeModes {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            VolumePermissions::ReadWrite => write!(f, "rw"),
-            VolumePermissions::ReadOnly => write!(f, "ro"),
+            VolumeModes::ReadWrite => write!(f, "rw"),
+            VolumeModes::ReadOnly => write!(f, "ro"),
         }
     }
 }
 
-impl FromStr for VolumePermissions {
+impl FromStr for VolumeModes {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self> {
         match s {
-            "rw" => Ok(VolumePermissions::ReadWrite),
-            "ro" => Ok(VolumePermissions::ReadOnly),
-            _ => Err(Error::invalid_value("volume permissions", s)),
+            "rw" => Ok(VolumeModes::ReadWrite),
+            "ro" => Ok(VolumeModes::ReadOnly),
+            _ => Err(Error::invalid_value("volume mode", s)),
         }
     }
 }
 
 #[test]
-fn volume_permissions_has_a_string_representation() {
+fn volume_mode_has_a_string_representation() {
     let pairs = vec![
-        (VolumePermissions::ReadWrite, "rw"),
-        (VolumePermissions::ReadOnly, "ro"),
+        (VolumeModes::ReadWrite, "rw"),
+        (VolumeModes::ReadOnly, "ro"),
     ];
     for (mode, s) in pairs {
         assert_eq!(mode.to_string(), s);
-        assert_eq!(mode, VolumePermissions::from_str(s).unwrap());
+        assert_eq!(mode, VolumeModes::from_str(s).unwrap());
     }
 }
 
